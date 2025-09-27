97b181ffa021c35746952efc54de757e971699b7
MukterHossain <mukter3075@gmail.com>
2025-09-27 22:16:54 +0600

    project setup done

---
+++ src/app.ts
@@ -0,0 +1,38 @@
+
+import cors from "cors";
+import express from "express";
+
+const app = express();
+
+// Middleware
+app.use(cors()); // Enables Cross-Origin Resource Sharing
+app.use(express.json()); // Parse incoming JSON requests
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
+  })
+);
+
+
+// app.use("/api/v1/user", UserRouter)
+// app.use("/api/v1/post", PostRouter)
+// app.use("/api/v1/auth", AuthRouter)
+
+
+// Default route for testing
+app.get("/", (_req, res) => {
+  res.send("API is running");
+});
+
+
+// 404 Handler
+app.use((req, res, next) => {
+  res.status(404).json({
+    success: false,
+    message: "Route Not Found",
+  });
+});
+
+export default app;
